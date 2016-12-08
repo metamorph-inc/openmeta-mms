2d198b2019e4c97d85f6b8140bb0a1b2976b6bf7
Jonathon Williams <jwilliams@metamorphsoftware.com>
2016-12-08 17:31:53 -0600

    Move PETConfig to AvmDdp lib

--- src/CyPhyPET/PET.cs
+++ src/CyPhyPET/PET.cs
@@ -17,54 +17,7 @@ namespace CyPhyPET
     using GME.MGA;
     using Newtonsoft.Json;
     using System.Globalization;
-
-    public class PETConfig
-    {
-        public class Parameter
-        {
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public string[] source;
-        }
-        public class Constraint
-        {
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public string[] source;
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public double? RangeMin;
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public double? RangeMax;
-        }
-        public class Component
-        {
-            public Dictionary<string, Parameter> parameters;
-            public Dictionary<string, Parameter> unknowns;
-            public Dictionary<string, string> details;
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public string type;
-        }
-        public class DesignVariable
-        {
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public string type;
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public double? RangeMin;
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public double? RangeMax;
-            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-            public List<object> items;
-        }
-        public class Driver
-        {
-            public string type;
-            public Dictionary<string, DesignVariable> designVariables;
-            public Dictionary<string, Parameter> objectives;
-            public Dictionary<string, Constraint> constraints;
-            public Dictionary<string, Parameter> intermediateVariables;
-            public Dictionary<string, object> details;
-        }
-        public Dictionary<string, Component> components;
-        public Dictionary<string, Driver> drivers;
-    }
+    using AVM.DDP;
 
     public class PET
     {
