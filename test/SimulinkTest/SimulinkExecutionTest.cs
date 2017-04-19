140adfb4503943281fccbc599dafac746b330c25
Jonathon Williams <jwilliams@metamorphsoftware.com>
2017-04-19 17:59:47 -0500

    Allow some tests to be skipped without MATLAB installed

---
+++ test/SimulinkTest/SimulinkExecutionTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GME.MGA;
+using SchematicUnitTests;
+using Xunit;
+
+namespace SimulinkTest
+{
+    public class SimulinkExecutionTest : InterpreterTestBaseClass, IUseFixture<InterpreterTestFixture>
+    {
+        #region Fixture
+        InterpreterTestFixture fixture;
+        public void SetFixture(InterpreterTestFixture data)
+        {
+            fixture = data;
+        }
+
+        public override MgaProject project
+        {
+            get
+            {
+                return fixture.proj;
+            }
+        }
+
+        public override String TestPath
+        {
+            get
+            {
+                return fixture.path_Test;
+            }
+        }
+        #endregion
+
+        [SkipWithoutMatlabFact]
+        public void PassTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ test/SimulinkTest/SkipWithoutMatlabFact.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Xunit;
+
+namespace SimulinkTest
+{
+    public class SkipWithoutMatlabFact : FactAttribute
+    {
+        public override string Skip
+        {
+            get
+            {
+                if (!IsMatlabInstalled)
+                {
+                    return "Matlab is not installed";
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            set { }
+        }
+
+        private bool? _isMatlabInstalled = null;
+        private bool IsMatlabInstalled
+        {
+            get
+            {
+                if (_isMatlabInstalled == null)
+                {
+                    // We consider MATLAB to be installed if its COM object is registered
+                    var matlabType = Type.GetTypeFromProgID("Matlab.Application");
+
+                    _isMatlabInstalled = !(matlabType == null);
+                }
+
+                return _isMatlabInstalled.Value;
+            }
+        }
+    }
+}
