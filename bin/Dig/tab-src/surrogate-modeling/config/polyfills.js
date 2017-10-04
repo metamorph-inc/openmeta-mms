63a74f68e41c3b4b9fe2498737852fb4ffa6ed3e
Jonathon Williams <jwilliams@metamorphsoftware.com>
2017-10-04 17:13:17 -0500

    Revert upgrade to React 16 due to broken react-bootstrap modals.
    (See Github issue https://github.com/react-bootstrap/react-bootstrap/issues/2812)
    
    This reverts commit d9b0d77e3f14c2d2619506958b2e3b87b8bb985d.

--- bin/Dig/tab-src/surrogate-modeling/config/polyfills.js
+++ bin/Dig/tab-src/surrogate-modeling/config/polyfills.js
@@ -15,9 +15,48 @@ require('whatwg-fetch');
 // It will use the native implementation if it's present and isn't buggy.
 Object.assign = require('object-assign');
 
-// ES6 Map and Set, required by React 16
-require('core-js/es6/map');
-require('core-js/es6/set');
+// Array.prototype.fill() polyfill from MDN:  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
+if (!Array.prototype.fill) {
+  Object.defineProperty(Array.prototype, 'fill', {
+    value: function(value) {
 
-// ES6 Array methods, required by our code
-require('core-js/es6/array');
+      // Steps 1-2.
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+
+      var O = Object(this);
+
+      // Steps 3-5.
+      var len = O.length >>> 0;
+
+      // Steps 6-7.
+      var start = arguments[1];
+      var relativeStart = start >> 0;
+
+      // Step 8.
+      var k = relativeStart < 0 ?
+        Math.max(len + relativeStart, 0) :
+        Math.min(relativeStart, len);
+
+      // Steps 9-10.
+      var end = arguments[2];
+      var relativeEnd = end === undefined ?
+        len : end >> 0;
+
+      // Step 11.
+      var final = relativeEnd < 0 ?
+        Math.max(len + relativeEnd, 0) :
+        Math.min(relativeEnd, len);
+
+      // Step 12.
+      while (k < final) {
+        O[k] = value;
+        k++;
+      }
+
+      // Step 13.
+      return O;
+    }
+  });
+}
