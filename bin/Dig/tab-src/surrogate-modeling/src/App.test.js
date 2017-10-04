10385dddc6e4904a6ee5f22fb9969100e4918e15
Jonathon Williams <jwilliams@metamorphsoftware.com>
2017-10-04 16:29:34 -0500

    Fix basic 'renders without crashing' test (part of template)

--- bin/Dig/tab-src/surrogate-modeling/src/App.test.js
+++ bin/Dig/tab-src/surrogate-modeling/src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import BackendService from './BackendService';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  const service = new BackendService();
+  ReactDOM.render(<App service={service} />, div);
 });
