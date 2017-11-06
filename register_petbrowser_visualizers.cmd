4061b5a36aa192edfc2ff5808cdffad2bd9736c9
Jonathon Williams <jwilliams@metamorphsoftware.com>
2017-11-06 17:49:08 -0600

    Add Open in Excel menu item

--- register_petbrowser_visualizers.cmd
+++ register_petbrowser_visualizers.cmd
@@ -4,3 +4,10 @@ reg add HKLM\SOFTWARE\META\PETBrowser\PETTools\ExportCSV /f /v "ExecutableFilePa
 reg add HKLM\SOFTWARE\META\PETBrowser\PETTools\ExportCSV /f /v "ProcessArguments" /d "\"%CD%\bin\PetBrowserExportCsv.py\" \"%%1\" \"%%4\" " /reg:32
 reg add HKLM\SOFTWARE\META\PETBrowser\PETTools\ExportCSV /f /v "ShowConsoleWindow" /d "0" /t REG_DWORD /reg:32
 reg add HKLM\SOFTWARE\META\PETBrowser\PETTools\ExportCSV /f /v "WorkingDirectory" /d "%%2" /reg:32
+
+reg add HKLM\SOFTWARE\META\PETBrowser\PETTools\OpenInExcel /f /ve /d "Open With Excel" /reg:32
+reg add HKLM\SOFTWARE\META\PETBrowser\PETTools\OpenInExcel /f /v "ActionName" /d "Open With Excel" /reg:32
+reg add HKLM\SOFTWARE\META\PETBrowser\PETTools\OpenInExcel /f /v "ExecutableFilePath" /d "%CD%\bin\Python27\Scripts\Python.exe" /reg:32
+reg add HKLM\SOFTWARE\META\PETBrowser\PETTools\OpenInExcel /f /v "ProcessArguments" /d "\"%CD%\bin\PetBrowserOpenInExcel.py\" \"%%1\" \"%%4\" " /reg:32
+reg add HKLM\SOFTWARE\META\PETBrowser\PETTools\OpenInExcel /f /v "ShowConsoleWindow" /d "0" /t REG_DWORD /reg:32
+reg add HKLM\SOFTWARE\META\PETBrowser\PETTools\OpenInExcel /f /v "WorkingDirectory" /d "%%2" /reg:32
