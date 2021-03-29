d2faad28a3ab5e03d98fe923f674dcd2a5fc41b5
Jonathon Williams <jwilliams@metamorphsoftware.com>
2021-03-29 17:36:16 -0500

    Update docs for opt_settings addition (OPENMETA-775)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -111,6 +111,11 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_context = {
+    'css_files': [
+        '_static/css/fix_long_table_lines.css',  # overrides for wide tables in RTD theme
+    ],
+}
 
 # -- Options for HTMLHelp output ------------------------------------------
 
